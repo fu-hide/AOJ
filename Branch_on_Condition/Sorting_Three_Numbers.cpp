76a05d07921ecca53a8e8219daa6c20dc02a9d9d
fu-hide <38994865+fu-hide@users.noreply.github.com>
2019-06-16 13:49:20 +0900

    Branch on Condition

--- Branch_on_Condition/Sorting_Three_Numbers.cpp
+++ Branch_on_Condition/Sorting_Three_Numbers.cpp
@@ -3,14 +3,29 @@
 using namespace std;
 
 int main(){
-    int a,b,c;
+    int a,b,c,tmp;
     cin >> a;
     cin >> b;
     cin >> c;
-    if((a < b) && (b < c)){
-      cout << "Yes" << endl;
-    }else{
-      cout << "No" << endl;
+
+    if(a > b){
+      tmp = b;
+      b = a;
+      a = tmp;
     }
+
+    if(b > c){
+      tmp = c;
+      c = b;
+      b = tmp;
+    }
+
+    if(a > b){
+      tmp = b;
+      b = a;
+      a = tmp;
+    }
+
+    cout << a << ' ' << b << ' ' << c << endl;
     return 0;
 }
