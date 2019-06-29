a66a6a34b42ed01df29fce79c44be4ff7027573f
fu-hide <38994865+fu-hide@users.noreply.github.com>
2019-06-29 15:36:43 +0900

    Spreadsheet

--- Structured_Program_II/Spreadsheet.cpp
+++ Structured_Program_II/Spreadsheet.cpp
@@ -3,12 +3,34 @@
 using namespace std;
 
 int main(){
-    int r, c;
+    int r, c, sum;
     cin >> r;
     cin >> c;
-    int vec[r][c];
+    int vec[r+1][c+1];
 
-    
+    for ( int row = 0; row < r; row++ ) {
+        sum = 0;
+        for ( int column = 0; column < c; column++ ) {
+            cin >> vec[row][column];
+            sum += vec[row][column];
+        }
+        vec[row][c] = sum; 
+    }
 
+    for ( int column = 0; column < c + 1; column++ ) {
+        sum = 0;
+        for ( int row = 0; row < r; row++ ) {
+            sum += vec[row][column];
+        }
+        vec[r][column] = sum;
+    }
+
+    for ( int row = 0; row < r + 1; row++ ) {
+        sum = 0;
+        for ( int column = 0; column < c; column++ ) {
+            cout << vec[row][column] << ' ';
+        }
+        cout << vec[row][c] << endl;
+    }  
     return 0;
 }
